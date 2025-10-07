8a613f9cc172f22b1e3c345e88937b4ca128b999
1lelanta <kekikoki4@gmail.com>
2025-10-07 19:30:47 +0300

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('mongoDB is connected');
+}).catch((err)=>{
+    console.log(err)
+});
 
 const PORT = process.env.PORT||3000;
 
