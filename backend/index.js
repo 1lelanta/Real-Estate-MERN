4ee52df2eea2813a1a817ed9e866484137d8acf5
1lelanta <kekikoki4@gmail.com>
2025-10-08 21:26:21 +0300

    fetch sign up api and show response on page

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import cors from 'cors'
 import dotenv from 'dotenv'
 import userRoute from './routes/userRoute.js';
 import userAuth from './controllers/signupController.js';
 dotenv.config();
 const app = express();
+app.use(cors())
 
 app.use(express.json())
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,20 +1,62 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    })
+    const data = await res.json()
+     console.log(data);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacit-95 disabled:opacity-95'>Sign Up</button>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          onChange={handleChange}
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          onChange={handleChange}
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          id='password'
+          onChange={handleChange}
+          className='border p-3 rounded-lg'
+        />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+          Sign Up
+        </button>
       </form>
-      <div className='flex'>
-        <p>Have an account ?</p>
-        <Link to={"/sign-in"}>
-        <span className='text-blue-700 gap-2 mt-5'>Sign In</span></Link>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>Sign In</span>
+        </Link>
       </div>
     </div>
   )
