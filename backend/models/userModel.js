647941494ca3613886cd0b9b49d2cd71b1869dd5
1lelanta <kekikoki4@gmail.com>
2025-10-11 20:40:47 +0300

    create an api for google sign in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,7 +14,13 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    avatar:{
+        type:String,
+        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fprofile-pic&psig=AOvVaw0L7t0PY8CjizLANOnvXHEK&ust=1760290175416000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKiNvonWnJADFQAAAAAdAAAAABAE"
+
+    },
+
 
 },{timestamps:true});
 
