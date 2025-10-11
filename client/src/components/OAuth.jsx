a9e79bfd10f9493cbe6c4cb69e0cd6f8801de03d
1lelanta <kekikoki4@gmail.com>
2025-10-11 18:51:07 +0300

    add google OAuth

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OAuth = () => {
+  return (
+     <button type='button' className='bg-red-700 text-white p-3 rounded-lg
+      uppercase hover:opacity-95'>
+    continue with google</button>
+  )
+}
+
+export default OAuth
