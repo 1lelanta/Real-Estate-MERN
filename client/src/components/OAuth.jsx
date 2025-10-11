221b7010df64688fb143b5fe362ef362791e559d
1lelanta <kekikoki4@gmail.com>
2025-10-11 19:04:57 +0300

    initialize firebase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const OAuth = () => {
+    const handleGoogleClick = async()=>{
+        try {
+            
+        } catch (error) {
+            console.log('could not sign in with google', error);
+            
+        }
+    }
+
+
   return (
      <button type='button' className='bg-red-700 text-white p-3 rounded-lg
       uppercase hover:opacity-95'>
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABUfGafEKvC-P5cYBBbMChcjIocBlOQrw",
+  authDomain: "lelantaestate.firebaseapp.com",
+  projectId: "lelantaestate",
+  storageBucket: "lelantaestate.firebasestorage.app",
+  messagingSenderId: "481448465351",
+  appId: "1:481448465351:web:e6ef5f28928a152c4dbfe2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
