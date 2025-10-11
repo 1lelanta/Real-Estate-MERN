11e796e99d564eb492287535e45c78c12aad8aaa
1lelanta <kekikoki4@gmail.com>
2025-10-11 19:55:20 +0300

    finish google sign in

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -19,7 +19,7 @@ const OAuth = () => {
         body:JSON.stringify({name:result.user.displayName, email:result.user.email, photo:result.user.photoURL})
       })
       const data = await res.json();
-      dispatch(signInSuccess);
+      dispatch(signInSuccess(data));
       
     } catch (error) {
       console.log('could not sign in with google', error);
