28835b30ffb4e19c90dbcd244dcca443df9e7296
1lelanta <kekikoki4@gmail.com>
2025-10-13 19:35:16 +0300

    finish profile picture

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 const Header = () => {
+    const {currentUser} = useSelector(state=>state.user);
   return (
     <header className='bg-slate-200 shadow-md '>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -25,10 +27,13 @@ const Header = () => {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About Us</li>
                 </Link>
-
-                <Link to='sign-in'>
+                {currentUser ?(
+                    <img className='rounded-full h-7 w-7
+                    object-cover' src={currentUser.avatar} alt="profile" />
+                ):<Link to='profile'>
                     <li className='text-slate-700 hover:underline'>Sign in</li>
-                </Link>
+                </Link>}
+                
                 
             </ul>
         </div>
