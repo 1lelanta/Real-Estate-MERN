2d98ce18fe5d9df8932da9ca9349488068f3cb20
1lelanta <kekikoki4@gmail.com>
2025-10-17 21:52:16 +0300

    fix a problem with listing api

--- backend/controllers/listingContoller.js
+++ backend/controllers/listingContoller.js
@@ -1,8 +1,9 @@
-import Listing from "../models/listingModel"
+import Listing from "../models/listingModel.js"
 
 export const createListing = async(req,res,next)=>{
     try {
-        const listing = await Listing.create(req.body)
+        const listing = await Listing.create(req.body);
+        return res.status(201).json(listing)
     } catch (error) {
         next(error)
     }
