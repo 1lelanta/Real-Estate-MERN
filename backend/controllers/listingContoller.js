207215e96063d82790942b5cee9ef939cee9b0c7
1lelanta <kekikoki4@gmail.com>
2025-10-17 21:50:46 +0300

    add listing controller api

--- backend/controllers/listingContoller.js
+++ backend/controllers/listingContoller.js
@@ -1,6 +1,8 @@
+import Listing from "../models/listingModel"
+
 export const createListing = async(req,res,next)=>{
     try {
-        
+        const listing = await Listing.create(req.body)
     } catch (error) {
         next(error)
     }
