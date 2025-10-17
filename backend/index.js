6bf099751a5c49394ec73cb9821a695c3c5aa98a
1lelanta <kekikoki4@gmail.com>
2025-10-17 22:01:52 +0300

    fix index api

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173', // your React app URL
-  credentials: true, // ✅ allow cookies / tokens to be sent
+  origin: 'http://localhost:5173', 
+  credentials: true, 
 }))
 
 app.use(express.json())
--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { createListing } from '../controllers/listingContoller.js';
-import { verify } from 'jsonwebtoken';
+
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
