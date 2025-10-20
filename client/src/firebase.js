a3077ef0f8379361132a72b561be446b79aab3a6
1lelanta <kekikoki4@gmail.com>
2025-10-20 09:29:47 +0300

    connect to supase for image storage

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "lelantaestate.firebaseapp.com",
   projectId: "lelantaestate",
-  storageBucket: "lelantaestate.firebasestorage.app",
+  storageBucket: "lelantaestate.appspot.com", // ✅ Corrected line
   messagingSenderId: "481448465351",
   appId: "1:481448465351:web:e6ef5f28928a152c4dbfe2"
 };
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 const CreateListing = () => {
+    const [files,setFiles] = useState([])
+    const handleImageSubmit = (e)=>{
+        if(files.length>0 && files.length<7){
+            const promises = [];
+
+            for(let i=0; i<files.length; i++){
+                promises.push(storeImage(files[i]))
+            };
+
+        };
+
+        const storeImage = async()=>{
+            return  new Promise((resolve,reject)=>{{
+                const storage = getStorage()
+            }})
+        }
+    }
   return (
     <main className='p-3 max-w-4xl mx-auto gap-4'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -71,8 +89,9 @@ const CreateListing = () => {
                         </span>
                     </p>
                     <div className='flex gap-4'>
-                        <input className='p-3 border border-gray-300 rounded w-full' type="file"  id='images' accept='image/*' multiple/>
-                        <button className='p-3 text-green-700 border border-green-700 rounded
+                        <input onChange={(e)=>setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file"  id='images' accept='image/*' multiple/>
+                        <button type='submit' onClick={handleImage}
+                         className='p-3 text-green-700 border border-green-700 rounded
                          uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
                         </div>
                         <button className='p-3 bg-slate-700 text-white rounded-lg uppercase font-bold
